07b1e23ee7de910edb3fde3df184ca17b931daf5
Shakhoyat <shakoyatsujon@gmail.com>
2025-08-13 17:51:36 +0600

    Refactor code structure for improved readability and maintainability

--- obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5745cc7688e9bf4e26a838294b23d8f1c60835c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08226a7d4d34b192747c223ac08ef16490b97d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
