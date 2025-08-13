85e5e958b3c2f2e5f2e0b32b3da5c7b6abea3781
Shakhoyat <shakoyatsujon@gmail.com>
2025-08-13 15:54:08 +0600

    Add Login page for admin access with validation and default credentials

--- obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b9e6e7e01364081efc80534a0cec69d53f3cbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94526f495a04f6e6d472ae3c614f31ab0c5b7bbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
