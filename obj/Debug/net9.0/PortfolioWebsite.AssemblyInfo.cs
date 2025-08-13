2881cce1901f0c8a959c884ee9d1062959567aad
Shakhoyat <shakoyatsujon@gmail.com>
2025-08-13 17:34:15 +0600

    Update AssemblyInfo and cache files; add Contact.cshtml and Gallery.cshtml for personal showcase

--- obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2689f6509ecd79c7baf40afce65ecd7c98039bbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f232ca71d62bb5f5a386979f06c9108ec1196577")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
