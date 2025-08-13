2e3e4e172e25365568bd17ce76825b8b727c6c45
Shakhoyat <shakoyatsujon@gmail.com>
2025-08-13 14:33:51 +0600

    Add DbContext and service registrations for application setup

--- Program.cs
+++ Program.cs
@@ -1,7 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using PortfolioWebsite.Data;
+using PortfolioWebsite.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => {
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+})
+.AddRoles<IdentityRole>()
+.AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddControllersWithViews();
+
+// Register custom services
+builder.Services.AddScoped<IPortfolioService, PortfolioService>();
+builder.Services.AddScoped<IGeminiAIService, GeminiAIService>();
+builder.Services.AddScoped<IGitHubService, GitHubService>();
+builder.Services.AddScoped<IScholarService, ScholarService>();
+
+// Add HTTP Client for API calls
+builder.Services.AddHttpClient();
+
+// Add memory cache for performance
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
