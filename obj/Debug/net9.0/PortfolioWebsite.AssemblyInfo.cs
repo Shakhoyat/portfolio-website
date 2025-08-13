d086895f008750ca9f4730cbdb6e3a7b3e1eeea9
Shakhoyat <shakoyatsujon@gmail.com>
2025-08-13 15:08:44 +0600

    Update assembly information and source link to reflect latest version

--- obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Debug/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85fecf4a3212301852f05672fc0c527869d8d14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c01fb824d37afa8920d7b87ca83d36d1cef92c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
