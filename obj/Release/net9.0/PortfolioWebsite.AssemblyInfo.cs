f19979913a2c093476dba22ddc4e9f6887d9b959
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-04 19:19:18 +0600

    Add assembly reference cache for updated NuGet packages in PortfolioWebsite project

--- obj/Release/net9.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Release/net9.0/PortfolioWebsite.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04692670f20b8d591ac17032b27923dfe4a9d68b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
